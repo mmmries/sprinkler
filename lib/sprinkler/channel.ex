58406e33656d4ffb9157cdf14d38625f5f25fdcc
Michael Ries <michael@riesd.com>
2018-09-27 07:58:46 -0600

    clarify that debug

--- lib/sprinkler/channel.ex
+++ lib/sprinkler/channel.ex
@@ -3,26 +3,26 @@ defmodule Sprinkler.Channel do
   require Logger
 
   def handle_in(event, payload, state) do
-    Logger.info("RECEIVED #{event} :: #{inspect payload}")
+    Logger.debug("RECEIVED #{event} :: #{inspect payload}")
     {:noreply, state}
   end
 
   def handle_reply({:timeout, :join}, state) do
-    Logger.info("JOIN TIMEOUT")
+    Logger.debug("JOIN TIMEOUT")
     Process.send_after(self(), :rejoin, 5_000)
     {:noreply, state}
   end
   def handle_reply({status, event, _ref}, state) do
-    Logger.info("REPLY #{status} :: #{event}")
+    Logger.debug("REPLY #{status} :: #{event}")
     {:noreply, state}
   end
   def handle_reply({status, event, payload, _ref}, state) do
-    Logger.info("REPLY #{status} :: #{event} :: #{inspect payload}")
+    Logger.debug("REPLY #{status} :: #{event} :: #{inspect payload}")
     {:noreply, state}
   end
 
   def handle_close(reason, state) do
-    Logger.info("Channel Closed #{inspect reason}")
+    Logger.debug("Channel Closed #{inspect reason}")
     Process.send_after(self(), :rejoin, 5_000)
     {:noreply, state}
   end
